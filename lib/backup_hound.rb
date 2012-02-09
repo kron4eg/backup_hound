c898d394534d8c3a9f64fab3d8340ed3b31c4c34
Artiom Di <kron82@gmail.com>
2012-02-09 20:54:27 +0200

    # -*- encoding: utf-8 -*-

--- lib/backup_hound.rb
+++ lib/backup_hound.rb
@@ -1,4 +1,7 @@
+# -*- encoding: utf-8 -*-
+
 require 'backup_hound/version'
+require 'backup_hound/dsl'
 
 module BackupHound
   # Your code goes here...
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# -*- encoding: utf-8 -*-
 
 require 'rubygems' if RUBY_VERSION < '1.9'
 require 'bundler/setup'
